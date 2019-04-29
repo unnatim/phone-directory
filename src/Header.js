c31ca69d8fc84bef4f9b592824ccc1499b685a34
unnatim <unnatiroks@yahoo.com>
2019-04-29 20:56:09 +0530

    Styled 'Header' (External Styling)

--- src/Header.js
+++ src/Header.js
@@ -1,21 +1,14 @@
 import React from 'react';
+import './Header.css';
+
 // In this line below, we import Component class to make Class components. But if no Class omponents //are required then this can be removed.
 //import React, {Component} from 'react';
 
 // This is Functional Component below.
 // The constant name Header refers to the File name Header. Hence both names should be same.
 const Header = function () {
-
-    const headerStyle = {
-        textAlign: 'center',
-        padding: 20,
-        background: '#000',
-        color: '#fff',
-        textTransform: 'uppercase'
-    };
-
     return (
-        <div style={headerStyle}>
+        <div className="header">
             Phone Directory
         </div>
     )
